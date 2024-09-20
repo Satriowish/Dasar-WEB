10665c111b78274223c410dbf24611fcc8e99251
Satriowish <satriowisnuap@gmail.com>
2024-09-20 09:12:28 +0700

    praktikum 3 soal 3.1

---
+++ Pertemuan4/operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+// Menampilkan Hasil 
+echo "Hasil Tambah = $a + $b = $hasilTambah<br>";
+echo "Hasil Kurang = $a - $b = $hasilKurang<br>";
+echo "Hasil Kali = $a * $b = $hasilKali<br>";
+echo "Hasil Bagi = $a / $b = $hasilBagi<br>";
+echo "Hasil sisaBagi = $a % $b = $sisaBagi<br>";
+echo "Hasil pangkat = $a ** $b = $pangkat<br>";
+?>
