90841860b5ddef3b994f327faea65b5c68197599
Satriowish <satriowisnuap@gmail.com>
2024-09-20 09:24:03 +0700

    praktikum 3 soal 3.2

--- Pertemuan4/operator.php
+++ Pertemuan4/operator.php
@@ -15,5 +15,21 @@ echo "Hasil Kurang = $a - $b = $hasilKurang<br>";
 echo "Hasil Kali = $a * $b = $hasilKali<br>";
 echo "Hasil Bagi = $a / $b = $hasilBagi<br>";
 echo "Hasil sisaBagi = $a % $b = $sisaBagi<br>";
-echo "Hasil pangkat = $a ** $b = $pangkat<br>";
+echo "Hasil pangkat = $a ** $b = $pangkat<br><br>";
+
+//Langkah 5
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama (a == b): " . ($hasilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Tidak Sama (a != b): " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil (a < b): " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar (a > b): " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Kecil Sama (a <= b): " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
+echo "Hasil Lebih Besar Sama (a >= b): " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br><br>";
+
 ?>
