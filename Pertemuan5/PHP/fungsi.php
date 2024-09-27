8087d7056192e72f1d7813bcc84bdc97f6ce2cf5
Satriowish <satriowisnuap@gmail.com>
2024-09-27 08:36:12 +0700

    Pertemuan 5 | PHP 2 | soal 5

--- Pertemuan5/PHP/fungsi.php
+++ Pertemuan5/PHP/fungsi.php
@@ -1,30 +1,19 @@
 <?php
 // Membuat Fungsi
-function perkenalan($nama, $salam = "Assalamualaikum") {
-    echo $salam.", ";
-    // echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan anda<br/>";
-}
-
-// Memanggil fungsi  yang sudah dibuat
-perkenalan("Satrio","Hallo");
-
-echo "<hr>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat Pagi";
-
-// Memanggilo Lagi
-perkenalan($saya);
-
-echo "<hr>";
-// Membuat fungsi
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam = "Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2004, 2024) ." tahun"; 
+    // Memanggil fungsi lain
+    echo "Umur Berusia ". hitungUmur(2004, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Satrio");
 
 ?>
