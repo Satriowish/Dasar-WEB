3069a074ece8642d3706e6333421f7b578a768f3
Satriowish <satriowisnuap@gmail.com>
2024-09-27 07:53:54 +0700

    Pertemuan 5 | PHP 2 | soal 1

---
+++ Pertemuan5/PHP/fungsi.php
@@ -0,0 +1,13 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Satrio<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// Memanggil fungsi  yang sudah dibuat
+perkenalan();
+perkenalan();
+
+?>
