c40b1b3f33f84510dd93c387d239f03b99e429ae
Satriowish <satriowisnuap@gmail.com>
2024-10-04 10:08:15 +0700

    Praktikum 3 Soal 4

---
+++ Pertemuan6/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Satrio<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+// Memanggil fungsi  yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
