18cc879db769313bd60a56df2f9db41b02638ea8
Satriowish <satriowisnuap@gmail.com>
2024-10-04 10:10:40 +0700

    Praktikum 3 Soal 5

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,10 +1,15 @@
 <?php
-function perkenalan() {
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Satrio<br/>";
+// Membuat Fungsi
+function perkenalan($nama, $salam) {
+    echo $salam.", ";
+    // echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan anda<br/>";
 }
-// Memanggil fungsi  yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Satrio","Hallo");
+echo "<hr>";
+$saya = "Elok";
+$ucapanSalam = "Selamat Pagi";
+// Memanggilo Lagi
+perkenalan($saya,$ucapanSalam);
 ?>
