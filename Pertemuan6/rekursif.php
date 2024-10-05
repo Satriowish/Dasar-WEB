3c7bf9c9883271e511b3b346f782c4b42cb406c1
Satriowish <satriowisnuap@gmail.com>
2024-10-05 19:16:40 +0700

    Soal 10

--- Pertemuan6/rekursif.php
+++ Pertemuan6/rekursif.php
@@ -1,8 +1,13 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo Dunia! <br>";
-    tampilkanHaloDunia();
-}
-
-tampilkanHaloDunia();
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-" . $indeks . "<br>";
+  
+    // Panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+      tampilkanAngka($jumlah, $indeks + 1); 
+  
+    }
+  }
+  
+  tampilkanAngka(20);
 ?>
