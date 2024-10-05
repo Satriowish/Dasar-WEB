67d4d46ace3e1e14f61740e44bd40d612682ef1f
Satriowish <satriowisnuap@gmail.com>
2024-10-05 19:20:56 +0700

    Soal 11

--- Pertemuan6/rekursif.php
+++ Pertemuan6/rekursif.php
@@ -1,13 +1,40 @@
 <?php
-function tampilkanAngka(int $jumlah, int $indeks = 1) {
-    echo "Perulangan ke-" . $indeks . "<br>";
-  
-    // Panggil diri sendiri selama $indeks <= $jumlah
-    if ($indeks < $jumlah) {
-      tampilkanAngka($jumlah, $indeks + 1); 
-  
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+                    ],[
+                        "nama" => "Kuliner"
+                    ],[
+                        "nama" => "Hiburan"
+                    ]
+        ]
+                    ],[
+                        "nama" => "Tentang"
+                    ],[
+                        "nama" => "Kontak"
+                    ],
+];
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}</li>";
+         
     }
-  }
-  
-  tampilkanAngka(20);
+    echo "</ul>";
+}
+tampilkanMenuBertingkat($menu);
 ?>
