f2b55a08e44c86743e26d22ba3ed09d795e84797
Satriowish <satriowisnuap@gmail.com>
2024-10-14 21:08:24 +0700

    Pertemuan 7 Soal 5.4

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -8,6 +8,7 @@ if (preg_match($pattern, $text)) {
     echo "tidak ada huruf kecil!";
     echo "<br>";
 }
+
 $pattern = '/[0-9]+/'; // cocokkan satu atau lebih digit
 $text = 'There are 123 apples..';
 if (preg_match($pattern, $text, $matches)) {
@@ -17,9 +18,20 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
     echo "<br>";
 }
+
 $pattern = '/apple/';
 $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // output: "i like banana pie"
 echo "<br>";
+
+$pattern = '/go*d/'; // cocokkan "god", "good", "gooood", dll.
+$text = 'god is good';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+    echo "<br>";
+} else {
+    echo "Tidak ada yang cocok!";
+    echo "<br>";
+}
