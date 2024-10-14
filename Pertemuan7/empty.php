33d8d1750a1006c73685d048d5bf032aa8b65018
Satriowish <satriowisnuap@gmail.com>
2024-10-14 20:36:49 +0700

    Pertemuan 7 Soal 2.2

--- Pertemuan7/empty.php
+++ Pertemuan7/empty.php
@@ -7,4 +7,13 @@ if (empty($myArray)) {
     echo "Array terdefinisi dan tidak kosong.";
     echo "<br>";
 }
+
+if (empty($nonExistentVar)) {
+    echo "Array tidak terdefinisi atau kosong";
+    echo "<br>";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+    echo "<br>";
+}
+
 ?>
