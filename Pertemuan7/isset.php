2767dc41ea67a56be4f65a11ef6ff722f4be3fa4
Satriowish <satriowisnuap@gmail.com>
2024-10-14 20:30:07 +0700

    Pertemuan 7 Soal 1.2

--- Pertemuan7/isset.php
+++ Pertemuan7/isset.php
@@ -5,5 +5,11 @@ if (isset($umur) && $umur >= 18) {
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
-
+echo "<br>";
+$data = array("nama" => "Satrio ", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
 ?>
