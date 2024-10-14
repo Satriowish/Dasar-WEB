84b60f818997573663f3cc1dc013ee0d204edb57
Satriowish <satriowisnuap@gmail.com>
2024-10-14 21:09:33 +0700

    Pertemuan 7 Soal 5.5

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -18,6 +18,11 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
     echo "<br>";
 }
+$pattern = '/go?d/'; // cocokkan "god", "good"
+$text = 'god is good';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+    echo "<br>";
 
 $pattern = '/apple/';
 $replacement = 'banana';
