a930330b2df32686e5f3516ce5a811dc4daf0a44
Satriowish <satriowisnuap@gmail.com>
2024-10-18 08:51:59 +0700

    Pertemuan 8 Soal 5

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,19 +1,20 @@
 <?php
 if(isset($_POST["submit"])){
-    $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
-
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
-        } else {
-            echo "Gagal mengunggah file.";
-        }
+  $target_dir = "uploads/"; // Direktori tujuan untuk menyimpan file
+  $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
+  $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+  $allowed_extensions = array("jpg", "jpeg", "png", "gif");
+  $max_size = 5 * 1024 * 1024; // 5 MB
+  
+  if (in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size) {
+    $namaFile = $_FILES["myfile"]["name"]; // untuk mendapatkan nama file yang telah diupload
+    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
+      echo "<img src='uploads/$namaFile' alt='' width='200px'> <br>"; // menampilkan gambar
+      echo "File berhasil diunggah.";
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+      echo "Gagal mengunggah file.";
     }
+  } else {
+    echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+  }
 }
-?>
