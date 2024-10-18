ca04626dc5c9166f79ede910d86fce15f4e09564
Satriowish <satriowisnuap@gmail.com>
2024-10-18 08:37:36 +0700

    Pertemuan 8 Soal 2

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    $target_dir = "uploads";
+    $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
     if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
         echo "File berhasil diunggah.";
