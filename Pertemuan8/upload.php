854a4964d3d57668566eca21a6793f6b11cfec75
Satriowish <satriowisnuap@gmail.com>
2024-10-23 07:04:47 +0700

    Pertemuan 8 Soal

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -3,8 +3,8 @@ if(isset($_POST["submit"])){
   $target_dir = "uploads/"; // Direktori tujuan untuk menyimpan file
   $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
   $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-  $allowed_extensions = array("jpg", "jpeg", "png", "gif");
-  $max_size = 5 * 1024 * 1024; // 5 MB
+  $allowed_extensions = array("txt", "pdf", "doc", "docx");
+  $max_size = 3 * 1024 * 1024; // 3 MB
   if (in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size) {
     $namaFile = $_FILES["myfile"]["name"]; // untuk mendapatkan nama file yang telah diupload
     if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
