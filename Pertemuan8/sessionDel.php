db18fa2c93242d0cf2930a040a69177c94b1b0e8
Satriowish <satriowisnuap@gmail.com>
2024-10-27 19:14:01 +0700

    Pertemuan 8 Soal 21

---
+++ Pertemuan8/sessionDel.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    // Kode untuk memanipulasi sesi, misalnya:
+    session_unset();
+    session_destroy();
+    echo "Semua variabel sesi sekarang telah dihapus, dan sesi telah dihancurkan.";
+    ?>
+</body>
+</html>
